ecea7f851a562d8ff44bda56ed56086791197cb0
Rupa <rupasaka91@gmail.com>
2026-08-05 21:15:02 +0530

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    pass
+    num1,num2 = map(int, input().split(','))
+    return num1+num2
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
