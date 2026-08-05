9e9b0dc9d1d203c7f65c39b6889f5ad88825beda
Rupa <rupasaka91@gmail.com>
2026-08-05 10:45:30 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "kanna"
+print(type(variable))
+variable = [2,'abd',3]
+print(type(variable))
